499f732a674de4c41df0041612dcacb888d84d51
Jeremy Hanmer <jeremy@dreamhost.com>
2022-09-21 14:47:05 -0700

    test

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -855,6 +855,14 @@ impl Process {
             .collect())
     }
 
+    pub fn ipaddr(&self) -> ProcResult<String> {
+        let mut s = String::new();
+        let mut f = FileWrapper::open_at(&self.root, &self.fd, "ipaddr")?;
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
+
+
     /// Returns the process ID for this process, if the process was created from an ID. Otherwise
     /// use stat().pid.
     pub fn pid(&self) -> i32 {
